454dc88947b307e8af2e95dc32112f37542c4e11
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-07-31 16:09:29 +0630

    server: auth > reset password user interface

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,7 @@ const app = express();
 // register middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("src/public"));
 
 app.use("/auth", authRouter);
 
