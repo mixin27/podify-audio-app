6737fa8f501c9bb8f32ae1b9ef79f87b70b4439c
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-08-07 14:05:40 +0630

    mobile: project setup

---
+++ PodifyPlayer/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  rules: {
+    // 'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'off',
+  },
+};
---
+++ PodifyPlayer/App.tsx
@@ -0,0 +1,9 @@
+import {SafeAreaView} from 'react-native';
+
+interface Props {}
+
+const App = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+export default App;
